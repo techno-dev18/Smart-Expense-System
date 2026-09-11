d7c3788f48bab22e889df9f966505ee3266056ce
techno-dev18 <technologix2op@gmail.com>
2026-09-11 21:00:02 +0530

    added pages

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+// Connect MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Smart Expense API is running",
+  });
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
